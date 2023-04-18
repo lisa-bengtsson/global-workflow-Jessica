d8c277db6d8cd9a92787baa1fb567c4e43dc94a1
Guillaume Vernieres <guillaume.vernieres@noaa.gov>
2023-04-17 20:04:36 -0400

    marine chkpt depd sfcana

--- workflow/rocoto/workflow_tasks.py
+++ workflow/rocoto/workflow_tasks.py
@@ -552,6 +552,8 @@ class Tasks:
         deps = []
         dep_dict = {'type': 'task', 'name': f'{self.cdump}ocnanalrun'}
         deps.append(rocoto.add_dependency(dep_dict))
+        dep_dict = {'type': 'task', 'name': f'{self.cdump}sfcanl'}
+        deps.append(rocoto.add_dependency(dep_dict))
         dependencies = rocoto.create_dependency(dep_condition='and', dep=deps)
 
         resources = self.get_resource('ocnanalchkpt')
